12f4d356a820ab0424a33b425bfefbef61f8d13e
yangchangxing <ycxusst@163.com>
2020-12-12 02:27:46 +0800

    快速开始完成,使用sql 可以查询出数据

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@MapperScan("com.example.demo.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,36 @@
 package com.example.demo;
 
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
+
+	@Autowired
+	private UserMapper userMapper;
 
 	@Test
 	void contextLoads() {
 		System.out.println(111);
+	}
+
+
+	@Test
+	public void testSelect() {
+		System.out.println(("----- selectAll method test ------"));
+		List<User> userList = userMapper.selectList(null);
+		System.out.println(userList);
+		//Assert.assertEquals(5, userList.size());
+		//userList.forEach(System.out::println);
 	}
 
 }
